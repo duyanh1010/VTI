341bb5d2e34a7928751b3e69256d12d325c212c0
duyanh1010 <duyanhr01@gmail.com>
2021-09-10 10:56:25 +0700

    Assignment

--- SQL/TestingSystem/Assignment2/Testing_System_Assignment_2.sql
+++ SQL/TestingSystem/Assignment2/Testing_System_Assignment_2.sql
@@ -4,7 +4,7 @@ USE Testing_System_Assignment_2;
 DROP TABLE IF EXISTS Department;
 CREATE TABLE Department(
 	 DepartmentID 	TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	 DepartmentName VARCHAR(50) UNIQUE KEY -- CHECK(length(FullName)>5)
+	 DepartmentName VARCHAR(50) UNIQUE KEY 
      
 );
 DROP TABLE IF EXISTS `Position`;
@@ -21,8 +21,8 @@ CREATE TABLE `Account`(
 	DepartmentID 	TINYINT 	UNSIGNED NOT NULL,
 	PositionID 		TINYINT 	UNSIGNED NOT NULL,
 	CreateDate 		DATETIME DEFAULT NOW(),
-    FOREIGN KEY (DepartmentID) 	REFERENCES  Department( DepartmentID),
-    FOREIGN KEY (PositionID) 	REFERENCES  `Position`(PositionID)
+    FOREIGN KEY (DepartmentID) 	REFERENCES  Department( DepartmentID) ON DELETE CASCADE,
+    FOREIGN KEY (PositionID) 	REFERENCES  `Position`(PositionID) ON DELETE CASCADE
     
 );
 DROP TABLE IF EXISTS `Group`;
@@ -31,7 +31,7 @@ CREATE TABLE `Group`(
 	GroupName 		VARCHAR(50)  UNIQUE KEY  ,
 	CreatorID 		INT UNSIGNED NOT NULL,
 	CreateDate 		DATETIME DEFAULT NOW(),
-    FOREIGN KEY (CreatorID) REFERENCES `Account`(AccountID)
+    FOREIGN KEY (CreatorID) REFERENCES `Account`(AccountID) ON DELETE CASCADE
 );
 DROP TABLE IF EXISTS GroupAccount;
 CREATE TABLE GroupAccount(
@@ -39,8 +39,8 @@ CREATE TABLE GroupAccount(
     AccountID		INT 	UNSIGNED,
     JoinDate		DATETIME	DEFAULT NOW(),
     PRIMARY KEY	(GroupID,AccountID),
-    FOREIGN KEY (GroupID) 	REFERENCES `Group`(GroupID),
-    FOREIGN KEY (AccountID) REFERENCES `Account`(AccountID)
+    FOREIGN KEY (GroupID) 	REFERENCES `Group`(GroupID) ON DELETE CASCADE,
+    FOREIGN KEY (AccountID) REFERENCES `Account`(AccountID) ON DELETE CASCADE
 );
 DROP TABLE IF EXISTS TypeQuestion ;
 CREATE TABLE TypeQuestion(
@@ -60,9 +60,9 @@ CREATE TABLE Question(
     TypeID			TINYINT UNSIGNED NOT NULL,
     CreatorID		INT 	UNSIGNED NOT NULL,
     CreateDate		DATETIME DEFAULT NOW(),
-    FOREIGN KEY (CategoryID) 	REFERENCES CategoryQuestion(CategoryID),
-    FOREIGN KEY (TypeID)		REFERENCES TypeQuestion(TypeID),
-    FOREIGN KEY (CreatorID)		REFERENCES `Account`(AccountID)
+    FOREIGN KEY (CategoryID) 	REFERENCES CategoryQuestion(CategoryID) ON DELETE CASCADE,
+    FOREIGN KEY (TypeID)		REFERENCES TypeQuestion(TypeID) ON DELETE CASCADE,
+    FOREIGN KEY (CreatorID)		REFERENCES `Account`(AccountID) ON DELETE CASCADE
 );
 DROP TABLE IF EXISTS  Answer ;
 CREATE TABLE Answer(
@@ -71,26 +71,26 @@ CREATE TABLE Answer(
     QuestionID		INT UNSIGNED NOT NULL,
     isCorrect		ENUM('T','F'),
     -- 				BIT
-    FOREIGN KEY (QuestionID) REFERENCES Question(QuestionID)
+    FOREIGN KEY (QuestionID) REFERENCES Question(QuestionID) ON DELETE CASCADE
 );
 DROP TABLE IF EXISTS Exam;
 CREATE TABLE Exam(
-	ExamID			INT 	UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    `Code`			INT 	UNSIGNED NOT NULL,
+	ExamID			INT 	UNSIGNED PRIMARY KEY  AUTO_INCREMENT ,
+    `Code`			CHAR(4) NOT NULL,
     Title			VARCHAR(100),
     CategoryID		TINYINT UNSIGNED NOT NULL,
     Duration 		TIME NOT NULL ,
     CreatorID		INT 	UNSIGNED NOT NULL,
     CreateDate		DATETIME	DEFAULT NOW(),
-    FOREIGN KEY (CategoryID) 	REFERENCES CategoryQuestion(CategoryID),
-	FOREIGN KEY (CreatorID)		REFERENCES `Account`(AccountID)
+    FOREIGN KEY (CategoryID) 	REFERENCES CategoryQuestion(CategoryID) ON DELETE CASCADE ,
+	FOREIGN KEY (CreatorID)		REFERENCES `Account`(AccountID) ON DELETE CASCADE
 );
 CREATE TABLE ExamQuestion(
 	ExamID			INT UNSIGNED,
     QuestionID		INT UNSIGNED,
-    PRIMARY KEY(ExamID,QuestionID),
-    FOREIGN KEY (ExamID)	REFERENCES Exam(ExamID),
-    FOREIGN KEY (QuestionID)REFERENCES Question(QuestionID)
+    PRIMARY KEY (ExamID,QuestionID),
+    FOREIGN KEY (ExamID)	REFERENCES Exam(ExamID) ON DELETE CASCADE,
+    FOREIGN KEY (QuestionID)REFERENCES Question(QuestionID) ON DELETE CASCADE
 );
 INSERT INTO Department(DepartmentName) 	VALUES ('Marketing'),('Sale'),('Bao Ve'),('Nhan Su'),('Ky Thuat'),
 											   ('Tai Chinh'),('Pho Giam Doc'),('Giam Doc'),('Thu Ky'),('Ban Hang');
@@ -102,39 +102,69 @@ VALUES ('duyanhr01@gmail.com','Anh','NguyenDuyAnh',1,1),
 	   ('trung@gmail.com','Trung','Phan Thai Trung',2,3),
        ('huong@gmail.com','Huong','Nguyen Duy Huong',3,1),
 	   ('linh@gmail.com','Linh','Phan My Linh',1,3),
-       ('hue@gmail.com','Hue','Nguyen Thi Hue',3,3),
-       ('kim@gmail.com','Kim','Kim Kim',2,2);
+       ('hue@gmail.com','Hue','Nguyen Hue',3,3),
+       ('kim@gmail.com','DuKimHo','Kim Kim',5,2),
+       ('hoa@gmail.com','hoavti','Nguyen Hoa',8,1),
+       ('lam@gmail.com','lamnd','Nguyen Thi Thanh Lam',7,2),
+       ('Hoang@gmail.com','Hoangpq','Phung Quynh Hoang',2,4),
+       ('Chung@gmail.com','ChungPK','Phung Kien Chung',6,3);
 -- SELECT*FROM `Account`;
-INSERT INTO `Group`(GroupName,CreatorID) 	VALUES ('ADN LAND',1), ('Home88',4), ('AD Homes',2),
-												   ('HD Group',3), ('Kim Cuong',6), ('Lap Trinh Vien',6);
+INSERT INTO `Group`(GroupName,CreatorID,CreateDate) 	
+VALUES 											   ('ADN LAND',1,'2019-12-20'), ('Home88',4,'2020-11-11'), ('AD Homes',2,'2018-2-2'),
+												   ('HD Group',3,'2019-2-3'), ('Kim Cuong',6,'2021-1-2'), ('Lap Trinh Vien',6,'2019-4-6'),
+                                                   ('Tuyen Dung',7,'2020-11-2'),('Ke Toan ',9,'2019-8-5'),('Bao ve',5,'2019-12-19'),('Noi Bo',6,'2019-12-18');
 -- SELECT*FROM `Group`;
 
-INSERT INTO GroupAccount(GroupID,AccountID) VALUES (1,1),(1,2),(2,2),(2,3),(3,1),(3,3),(4,3),(5,5),(6,1);
+INSERT INTO GroupAccount(GroupID,AccountID) VALUES (1,1),(1,2),(2,2),(2,3),(3,1),(3,3),(4,3),(5,5),(6,1),(7,8);
+
 -- SELECT*FROM  GroupAccount;
 INSERT INTO TypeQuestion(TypeName) 			VALUES ('Essay'),('Multiple-Choice');
 -- SELECT *FROM TypeQuestion;
-INSERT INTO CategoryQuestion(CategoryName) 	VALUES ('Java'),('SQL'),('Ruby'),('Postman'),('PHP'),('Nodejs');
+INSERT INTO CategoryQuestion(CategoryName) 	VALUES ('Java'),('SQL'),('Ruby'),('Postman'),('PHP'),
+												   ('Nodejs'),('React js'),('Mongo db'),('Python'),('Blockchain');
 -- SELECT*FROM CategoryQuestion;
-INSERT INTO Question(Content,CategoryID,TypeID,CreatorID) VALUES ('cau hoi Java day la noi dung content dau tien ',1,1,2),
-																 ('tai sao ban hoc SQL day la noi dung content   ',2,2,2),
-                                                                 ('tai sao ban hoc Ruby day la noi dung content   ',3,1,2),
-                                                                 ('tai sao ban hoc Postman day la noi dung content   ',4,2,1),
-                                                                 ('tai sao ban hoc PHP day la noi dung content ',5,1,1),
-                                                                 ('tai sao ban hoc Nodejs day la noi dung content  ',6,2,2);
+INSERT INTO Question(Content,CategoryID,TypeID,CreatorID)VALUES ('cau hoi Java day la noi dung content dau tien ',1,1,2),
+																('tai sao ban hoc SQL day la noi dung content   ',2,2,2),
+																('tai sao ban hoc Ruby day la noi dung content   ',3,1,2),
+																('tai sao ban hoc Postman day la noi dung content   ',4,2,1),
+																('cau hoi ban hoc PHP day la noi dung content ',5,1,1),
+																('tai sao ban hoc Nodejs day la noi dung content  ',6,2,2);
+																
+                                                               
+INSERT INTO Question(Content,CategoryID,TypeID,CreatorID,CreateDate)
+VALUES 															('tai sao ban hoc Blockchain day la noi dung content  ',9,1,6,'2019-12-12'),
+																('tai sao ban hoc React js day la noi dung content  ',8,2,4,'2019-1-1'),
+																('tai sao ban hoc Mongo db day la noi dung content  ',9,1,8,'2020-2-2'),
+																('tai sao ban hoc Python day la noi dung content  ',5,2,5,'2019-8-9');
 -- SELECT*FROM Question;
-INSERT INTO Answer(Content,QuestionID,isCorrect) VALUES ('Java day la noi dung cau tra loi cho cau hoi cau hoi ',1,'T'),
-														('SQL day la noi dung cau tra loi cho cau hoi sao ban hoc ',2,'F'),
-                                                        ('Ruby day la noi dung cau tra loi cho cau hoi sao ban hoc ',3,'F'),
-                                                        ('Postman day la noi dung cau tra loi cho cau hoi sao ban hoc ',4,'T'),
-                                                        ('PHP day la noi dung cau tra loi cho cau hoi sao ban hoc',5,'F'),
-														('Nodejsday la noi dung cau tra loi cho cau hoi sao ban hoc   ',6,'T');
+INSERT INTO Answer(Content,QuestionID,isCorrect) 		 VALUES ('Java day la noi dung cau tra loi cho cau hoi cau hoi ',1,'T'),
+																('SQL day la noi dung cau tra loi cho cau hoi sao ban hoc ',2,'F'),
+																('Ruby day la noi dung cau tra loi cho cau hoi sao ban hoc ',3,'F'),
+																('Postman day la noi dung cau tra loi cho cau hoi sao ban hoc ',4,'T'),
+																('PHP day la noi dung cau tra loi cho cau hoi sao ban hoc',5,'F'),
+																('Nodejs day la noi dung cau tra loi cho cau hoi sao ban hoc   ',7,'T'),
+																('React js day la noi dung cau tra loi cho cau hoi sao ban hoc   ',6,'T'),
+																('Mongo db  day la noi dung cau tra loi cho cau hoi sao ban hoc   ',8,'T'),
+																('Python  day la noi dung cau tra loi cho cau hoi sao ban hoc   ',9,'T'),
+																('Blockchain 1 la noi dung cau tra loi cho cau hoi sao ban hoc   ',10,'T'),
+                                                                ('Blockchain 2 la noi dung cau tra loi cho cau hoi sao ban hoc   ',10,'F'),
+                                                                ('Blockchain 3 la noi dung cau tra loi cho cau hoi sao ban hoc   ',10,'F'),
+                                                                ('Blockchain 4 la noi dung cau tra loi cho cau hoi sao ban hoc   ',10,'F'),
+                                                                ('Blockchain 5 la noi dung cau tra loi cho cau hoi sao ban hoc   ',10,'F');
 -- SELECT*FROM Answer;
-INSERT INTO Exam(`Code`,Title,CategoryID,Duration,CreatorID) VALUES (11,'Tieu de bai thi 11 ', 2,3000,2),
-																	(12,'Tieu de bai thi 12 ', 4,2000,1),
-                                                                    (13,'Tieu de bai thi 13 ', 1,5000,3),
-                                                                    (14,'Tieu de bai thi 14 ', 3,3000,5),
-                                                                    (15,'Tieu de bai thi 15 ', 3,1000,5),
-                                                                    (16,'Tieu de bai thi 16 ', 4,4000,2);
+INSERT INTO Exam(`Code`,Title,CategoryID,Duration,CreatorID) 
+VALUES 															('11AE','Tieu de bai thi 11 ', 2,3000,2),
+																('PC11','Tieu de bai thi 12 ', 4,20000,1),
+																('F855','Tieu de bai thi 13 ', 1,5000,3),
+																('HH04','Tieu de bai thi 14 ', 3,3000,5),
+																('S102','Tieu de bai thi 15 ', 3,10000,5),
+																('992E','Tieu de bai thi 16 ', 4,4000,8),
+																('UQ21','Tieu de bai thi 17 ', 4,5000,9);
+INSERT INTO Exam(`Code`,Title,CategoryID,Duration,CreatorID,CreateDate)
+VALUES 									
+																('DA88','Tieu de bai thi 18 ', 4,55000,10,'2019-12-20'),
+																('HA67','Tieu de bai thi 19 ', 4,40000,2,'2018-1-1'),
+																('QP12','Tieu de bai thi 20 ', 4,10000,4,'2019-12-10');
  SELECT*FROM Exam;
-INSERT INTO ExamQuestion(ExamID,QuestionID) VALUES (1,2),(1,3),(2,2),(3,4),(2,3),(5,6),(5,5);
+INSERT INTO ExamQuestion(ExamID,QuestionID) VALUES (1,2),(1,3),(2,2),(3,4),(2,3),(5,6),(5,5),(10,10),(2,7),(6,6),(8,9),(8,6);
 -- SELECT*FROM ExamQuestion;
---
+++ SQL/TestingSystem/Assignment3/Testing_System_Assignment_3.sql
@@ -0,0 +1,84 @@
+USE Testing_System_Assignment_2;
+-- Question 1: Thêm ít nhất 10 record vào mỗi table đã thêm ở Testing_System_Assignment_2;
+-- Question 2: lấy ra tất cả các phòng ban
+SELECT*FROM Department;
+-- Question 3: lấy ra id của phòng ban "Sale"
+SELECT 
+    DepartmentID
+FROM
+    Department
+WHERE
+    DepartmentName = 'Sale';
+-- Question 4: lấy ra thông tin account có full name dài nhất
+SELECT 
+    *
+FROM
+    `Account`
+WHERE
+    LENGTH(FullName) = (SELECT 
+							MAX(LENGTH(FullName))
+						FROM
+							`Account`);
+-- cách 2
+SELECT 
+    *
+FROM
+    `Account`
+ORDER BY LENGTH(FullName) DESC
+LIMIT 1;
+-- Question 5: Lấy ra thông tin account có full name dài nhất và thuộc phòng ban có id= 3
+WITH de as (SELECT*FROM `Account` WHERE DepartmentID=3 )
+SELECT *FROM de WHERE length(FullName)=(SELECT MAX(length(FullName)) FROM de);
+-- cách 2 
+SELECT 
+    *
+FROM
+    `Account`
+WHERE
+    DepartmentID = 3
+ORDER BY LENGTH(FullName) DESC
+LIMIT 1;
+-- Question 6: Lấy ra tên group đã tham gia trước ngày 20/12/2019
+SELECT 
+    *
+FROM
+    `Group`
+WHERE
+    CreateDate < '2019-12-20';
+-- Question 7: Lấy ra ID của question có >= 4 câu trả lời
+SELECT 
+    QuestionID
+FROM
+    Answer
+GROUP BY QuestionID
+HAVING COUNT(QuestionID) >= 4;
+-- cách 2 
+SELECT 
+    QuestionID,GROUP_CONCAT(AnswerID SEPARATOR ',') AS Answer4
+FROM
+    Answer
+GROUP BY QuestionID
+HAVING COUNT(QuestionID) >= 4;
+
+-- Question 8: Lấy ra các mã đề thi có thời gian thi >= 60 phút và được tạo trước ngày
+-- 20/12/2019
+SELECT*FROM Exam WHERE Duration>=10000 AND CreateDate<'2019-12-20';
+-- Question 9: Lấy ra 5 group được tạo gần đây nhất
+SELECT*FROM `group` ORDER BY CreateDate DESC LIMIT 5;
+-- Question 10: Đếm số nhân viên thuộc department có id = 2
+SELECT COUNT(AccountID)  FROM `Account` WHERE DepartmentID=2;
+-- Question 11: Lấy ra nhân viên có tên bắt đầu bằng chữ "D" và kết thúc bằng chữ "o"
+SELECT*FROM `Account` WHERE Username LIKE'D%o';
+-- Question 12: Xóa tất cả các exam được tạo trước ngày 20/12/2019
+DELETE FROM Exam WHERE CreateDate <'2019-12-20';
+-- Question 13: Xóa tất cả các question có nội dung bắt đầu bằng từ "câu hỏi"
+DELETE FROM Question WHERE Content LIKE 'cau hoi%';
+-- Question 14: Update thông tin của account có id = 5 thành tên "Nguyễn Bá Lộc" và
+-- email thành loc.nguyenba@vti.com.vn
+UPDATE `Account` SET FullName='Nguyễn Bá Lộc',Email='loc.nguyenba@vti.com.vn' WHERE AccountID=5;
+SELECT*FROM `Account`;
+-- Question 15: update account có id = 5 sẽ thuộc group có id = 4
+UPDATE `GroupAccount` SET  AccountID=5 WHERE GroupID=4 ;
+SELECT 	*
+From 	GroupAccount
+where	GroupID = 4;
